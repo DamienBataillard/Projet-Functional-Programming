b5b522aebcff85d0bf552536b70fbc4175cf33a2
Kamilia <kamilia.benkirane.stagiaire@elysee.fr>
2024-07-03 16:00:16 +0200

    test succeed

---
+++ scala3_project/src/main/scala/DirectedGraph.scala
@@ -0,0 +1,54 @@
+//
+case class DirectedGraph[T](vertices: Set[T], edges: Set[(T, T)]) extends Graph[T] {
+  def getVertices(): Set[T] = vertices
+
+  def getEdges(): Set[(T, T)] = edges
+
+  def getNeighbors(vertex: T): Set[T] = {
+    edges.collect { case (v1, v2) if v1 == vertex => v2 }
+  }
+
+  def addEdge(edge: (T, T)): DirectedGraph[T] = {
+    copy(edges = edges + edge)
+  }
+
+  def removeEdge(edge: (T, T)): DirectedGraph[T] = {
+    copy(edges = edges - edge)
+  }
+
+  def toDot: String = {
+    val edgeStrings = edges.map { case (v1, v2) => s"""  "$v1" -> "$v2"""" }.mkString(";\n")
+    s"""digraph G {
+       |$edgeStrings
+       |}""".stripMargin
+  }
+
+  def dfs(start: T): List[T] = {
+    def dfsRecursive(vertex: T, visited: Set[T]): (List[T], Set[T]) = {
+      if (visited.contains(vertex)) (List(), visited)
+      else {
+        val neighbors = getNeighbors(vertex)
+        val (result, newVisited) = neighbors.foldLeft((List(vertex), visited + vertex)) { case ((res, vis), neighbor) =>
+          val (subRes, subVisited) = dfsRecursive(neighbor, vis)
+          (res ++ subRes, subVisited)
+        }
+        (result, newVisited)
+      }
+    }
+    dfsRecursive(start, Set())._1
+  }
+
+  def bfs(start: T): List[T] = {
+    def bfsIterative(queue: List[T], visited: Set[T], result: List[T]): List[T] = {
+      if (queue.isEmpty) result
+      else {
+        val current = queue.head
+        val neighbors = getNeighbors(current).filterNot(visited.contains)
+        bfsIterative(queue.tail ++ neighbors, visited + current, result :+ current)
+      }
+    }
+    bfsIterative(List(start), Set(start), List())
+  }
+
+
+}
--- scala3_project/src/test/scala/GraphSpec.scala
+++ scala3_project/src/test/scala/GraphSpec.scala
@@ -16,16 +16,17 @@ class GraphSpec extends AnyFlatSpec with Matchers {
   }
 
   it should "perform DFS correctly" in {
-    val graph = DirectedGraph(Set(1, 2, 3, 4), Set((1, 2), (1, 3), (2, 4), (3, 4)))
-    val dfsResult = graph.dfs(1)
-    dfsResult shouldEqual List(1, 2, 4, 3)
+    val graph = DirectedGraph(Set(0,1, 2, 3, 4,5), Set((0, 1), (0, 2), (1, 5),(1, 4),(2, 3)))
+    val dfsResult = graph.dfs(0)
+    dfsResult should (equal(List(0, 1, 5, 4, 2, 3)) or equal(List(0, 1, 4, 5, 2, 3)))
   }
 
   it should "perform BFS correctly" in {
-    val graph = DirectedGraph(Set(1, 2, 3, 4), Set((1, 2), (1, 3), (2, 4), (3, 4)))
-    val bfsResult = graph.bfs(1)
-    bfsResult shouldEqual List(1, 2, 3, 4)
-  }
+  val graph = DirectedGraph(Set(0,1, 2, 3, 4,5), Set((0, 1), (0, 2),(1,3), (2, 4), (2,5)))
+  val bfsResult = graph.bfs(0)
+  bfsResult should (equal(List(0, 1, 2, 3, 4, 5)) or equal(List(0, 1, 2, 3, 5, 4)))
+}
+
 
   "An UndirectedGraph" should "correctly add and remove edges" in {
     val graph = UndirectedGraph(Set(1, 2, 3), Set((1, 2)))
@@ -46,7 +47,7 @@ class GraphSpec extends AnyFlatSpec with Matchers {
   }
 
   it should "perform BFS correctly" in {
-    val graph = UndirectedGraph(Set(1, 2, 3, 4), Set((1, 2), (1, 3), (2, 4), (3, 4)))
+    val graph = UndirectedGraph(Set(1, 2, 3, 4), Set((1, 2), (1, 3), (2, 4)))
     val bfsResult = graph.bfs(1)
     bfsResult shouldEqual List(1, 2, 3, 4)
   }
@@ -70,7 +71,7 @@ class GraphSpec extends AnyFlatSpec with Matchers {
   }
 
   it should "perform BFS correctly" in {
-    val graph = WeightedGraph(Set(1, 2, 3, 4), Map((1, 2) -> 5, (1, 3) -> 3, (2, 4) -> 8, (3, 4) -> 2))
+    val graph = WeightedGraph(Set(1, 2, 3, 4), Map((1, 2) -> 5, (1, 3) -> 3, (2, 4) -> 8))
     val bfsResult = graph.bfs(1)
     bfsResult shouldEqual List(1, 2, 3, 4)
   }
